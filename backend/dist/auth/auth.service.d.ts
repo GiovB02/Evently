5108c9fcdd744c52231f9bff53f996e167be9162
Melanie Giovanna Valle Benavides <20225407@esen.edu.sv>
2025-10-20 06:51:27 -0600

    Moved backend to another repository

--- backend/dist/auth/auth.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../models/evently.models';
-export declare class AuthService {
-    register(email: string, password: string, displayName: string): Promise<User>;
-    login(email: string, password: string): Promise<{
-        token: string;
-    }>;
-}
--- backend/src/organizer/organizer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrganizerService } from './organizer.service';
-import { OrganizerController } from './organizer.controller';
-
-@Module({
-  providers: [OrganizerService],
-  controllers: [OrganizerController]
-})
-export class OrganizerModule {}
